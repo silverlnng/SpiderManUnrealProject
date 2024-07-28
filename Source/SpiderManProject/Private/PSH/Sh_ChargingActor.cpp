8c48a4188b22862b85eb6e5d21c29b3c85f9614d
ParkSoonHong <95293014+ParkSoonHong@users.noreply.github.com>
2024-07-28 23:07:02 +0900

    [feat] 임시 Slash Efeect
    [feat] 임시 Slash Efeect

---
+++ Source/SpiderManProject/Private/PSH/Sh_ChargingActor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PSH/Sh_ChargingActor.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ASh_ChargingActor::ASh_ChargingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	col = CreateDefaultSubobject<UBoxComponent>(TEXT("Col"));
+	SetRootComponent(col);
+	col->SetBoxExtent(FVector(30, 150, 40));
+	col->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	mesh->SetupAttachment(RootComponent);
+	mesh->SetRelativeLocation(FVector(-230, 0, 0));
+	mesh->SetRelativeRotation(FRotator(0,-90,0));
+	mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	ConstructorHelpers::FObjectFinder<UStaticMesh>tempMesh(TEXT("/Script/Engine.StaticMesh'/Game/NiagaraMagicalSlashes/Model/SM_Slash_02.SM_Slash_02'"));
+
+	if (tempMesh.Succeeded())
+	{
+		mesh->SetStaticMesh(tempMesh.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void ASh_ChargingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetLifeSpan(5);
+}
+
+// Called every frame
+void ASh_ChargingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetActorLocation(GetActorLocation() + GetActorForwardVector() * speed * DeltaTime);
+}
+
+
+
---
+++ Source/SpiderManProject/Public/PSH/Sh_ChargingActor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Sh_ChargingActor.generated.h"
+
+UCLASS()
+class SPIDERMANPROJECT_API ASh_ChargingActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASh_ChargingActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent * col;
+	UPROPERTY(EditAnywhere)
+	float speed = 1000;
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent * mesh;
+};
